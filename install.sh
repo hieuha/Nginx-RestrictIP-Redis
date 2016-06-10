d36c65d11af3016d17da4bd39c6efc14a3571d54
Hieu Ha-Trung <hieuht@vnoss.org>
2016-06-10 11:48:02 +0700

    update

--- install.sh
+++ install.sh
@@ -4,90 +4,16 @@
 ### Centos / Redhat
 
 # Install library
+apt-get install -y lua5.1 liblua5.1-0 liblua5.1-0-dev
 
 # Clone Module
-
+git clone https://github.com/openresty/lua-nginx-module.git
+git clone https://github.com/openresty/set-misc-nginx-module.git
+git clone https://github.com/simpl/ngx_devel_kit.git
 # Compile
+./configure --prefix=/usr/share/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --lock-path=/var/lock/nginx.lock --pid-path=/run/nginx.pid --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --with-debug --with-pcre-jit --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-http_auth_request_module --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_image_filter_module --with-http_sub_module --with-http_xslt_module --with-mail --with-mail_ssl_module --add-module=/usr/src/ModSecurity-nginx_refactoring/nginx/modsecurity --with-ld-opt=-Wl,-E --add-module=/opt/sources/ngx_devel_kit --add-module=/opt/sources/set-misc-nginx-module --add-module=/opt/sources/lua-nginx-module
 
 # Install
+make 
+make install
 
-
-cd /opt/sources/
-
-
-git clone https://github.com/openresty/lua-nginx-module.git
-
-git clone https://github.com/openresty/set-misc-nginx-module.git
-
-git clone https://github.com/simpl/ngx_devel_kit.git
-
-
-cd /opt/sources/nginx-1.10.1
-
-./configure --prefix=/opt/nginx --with-http_stub_status_module --with-http_realip_module --user=www-data --group=www-data --without-http_uwsgi_module --without-http_scgi_module --with-http_gzip_static_module --without-http_ssi_module --without-http_geo_module --without-http_map_module --with-http_perl_module  --with-http_gzip_static_module --with-http_secure_link_module  --with-http_ssl_module --add-module=/opt/sources/ngx_devel_kit --add-module=/opt/sources/modsecurity-2.9.1/nginx/modsecurity --add-module=/opt/sources/set-misc-nginx-module --add-module=/opt/sources/lua-nginx-module
-
-
-
-=============================
-
-worker_processes  1;
-
-
-events {
-
-    worker_connections  1024;
-
-}
-
-
-http {
-
-    include       mime.types;
-
-    default_type  application/octet-stream;
-
-    sendfile        on;
-
-
-    keepalive_timeout  65;
-
-    # Mod Security
-
-    ModSecurityEnabled on;
-
-    ModSecurityConfig modsecurity.conf;
-
-
-    server {
-
-        listen       80;
-
-        server_name  localhost;
-
-        set $badurl "$http_host$request_uri";
-
-        set_encode_base64 $badurl;
-
-        location / {
-
-            root   html;
-
-            index  index.html index.htm;
-
-        }
-
-        error_page 403 /page-403.html;
-
-        location = /page-403.html {
-
-            allow all;
-
-            return 302 http://analytics.admicro.local/403.html?badurl=$badurl;
-
-        }
-
-
-    }
-
-
-}
