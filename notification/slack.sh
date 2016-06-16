68d428cdcd1ea56d5b0901523940636848e4a1e0
Hieu Ha-Trung <hieuht@vnoss.org>
2016-06-16 11:19:11 +0700

    truncate sms.sh

--- notification/slack.sh
+++ notification/slack.sh
@@ -1,1 +1,1 @@
-curl -X POST --data-urlencode 'payload={"channel": "#alert", "username": "webhookbot", "text": "This is posted to #alert and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+curl -s -X POST --data-urlencode 'payload={"channel": "#alert", "username": "Bulldog", "text": "", "icon_emoji": ":dog:"}' https://hooks.slack.com/services/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 > /dev/null 2>&1
--- notification/sms.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST --data-urlencode 'payload={"channel": "#alert", "username": "webhookbot", "text": "SMS This is posted to #alert and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
